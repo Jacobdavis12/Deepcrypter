7c59dfb07c9733bb99cae63666dd1e87aa673542
Jacob Davis <jacobdavis799@gmail.com>
2024-05-04 07:37:54 +0100

    Presentation standard

---
+++ class.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 28 06:12:33 2024
+
+@author: jacob
+"""
+import pandas
+
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler
+import torch.optim as optim
+
+import numpy as np
+
+import time
+import math
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import matplotlib
+import matplotlib as mpl
+
+
+#Local Modules
+from ciphers import genericSubstitution, vigenere, substitution, column
+from utils import alphaspacelower, alphabetEmbedder, dealphabetEmbedder, oneHotEncoder,deOneHotEncoder, plotConfusion
+from classModel import Classifier, classifierDataLoader
+
+print('p')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#device = 'cpu'
+print(device)
+
+## Load data
+length = 51043
+classes = ['substitution', 'vigenere', 'column']
+#X, Y = generateData(device, alphaspacelower, [substitution, vigenere, column], length = length)
+
+# print(next(Y[0]))
+# print(next(Y[1]))
+# print(next(Y[2]))
+# print(X[0])
+# del X[0]
+
+## format into dataloader
+# x = [item for sublist in Y for item in sublist]
+# label = np.arange(3)
+# label = np.tile(label, (length, 1))
+# label = label.T.flatten()
+
+# trainData, testData = classifierDataLoader(x, label, oneHotEncoder, 0.8, device = device)
+# torch.save(trainData, 'trainData.pt')
+# torch.save(testData, 'testData.pt')
+trainData = torch.load('trainData.pt')
+testData = torch.load('testData.pt')
+print('gg')
+
+trainloader = DataLoader(trainData, batch_size=30)
+print('loadedF')
+
+## train model
+classifier = Classifier(102*length, 3).to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(classifier.parameters(), lr=0.001, momentum=0.9)
+
+losses = []
+for epoch in range(15):  # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = classifier(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 500 == 499:    # print every 2000 mini-batches
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 200:.3f}')
+            losses.append([running_loss])
+            running_loss = 0.0
+
+torch.save(classifier.state_dict(), 'classifierTrans1.pth')
+
+t = np.arange(len(losses))
+
+fig, ax = plt.subplots()
+ax.plot(t, losses)
+
+ax.set(xlabel='iteration', ylabel='loss',
+       title='Loss plot')
+ax.grid()
+
+fig.savefig("lossClass1.png")
+plt.show()
+
+print('Finished Training')
+
+#classifier = Classifier(102*length, 3).to(device)
+#classifier.load_state_dict(torch.load('classifier.pth'))
+
+##Test
+confusionMatrix = classifier.test(testData, classes)
+
+plotConfusion(confusionMatrix, classes)
+
+
+
+print('end')
---
+++ transformerModel.py
@@ -0,0 +1,289 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 28 06:12:33 2024
+
+@author: jacob
+"""
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler
+
+import math
+import time
+
+import matplotlib
+
+def transformDataLoader(X, y, embedder, sos, eos, pad, batchSize, device):
+    X = list(embed(X, embedder, sos, eos, pad))
+    y = list(embed(list(y), embedder, sos, eos, pad))
+    data = TensorDataset(torch.LongTensor(X).to(device),
+                         torch.LongTensor(y).to(device))
+    return DataLoader(data, batch_size=batchSize)
+
+def embed(dataset, embedder, sos, eos, pad):
+    length = max([len(data) for data in dataset])
+    for data in dataset:
+        yield [sos] + [embedder(value) for value in data] + (length- len(data))*[pad] + [eos]
+
+def deembed(dataset, deembedder):
+    for data in dataset:
+        yield ''.join([deembedder(value) for value in data])
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, dropout_p=0.1):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, batch_first=True)
+        self.dropout = nn.Dropout(dropout_p)
+
+    def forward(self, input):
+        embedded = self.dropout(self.embedding(input))
+        output, hidden = self.gru(embedded)
+        return output, hidden
+
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, device, MAX_LENGTH, SOS_token):
+        super(DecoderRNN, self).__init__()
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, batch_first=True)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.device = device
+        self.MAX_LENGTH = MAX_LENGTH
+        self.SOS_token = SOS_token
+
+    def forward(self, encoder_outputs, encoder_hidden, target_tensor=None):
+        batch_size = encoder_outputs.size(0)
+        decoder_input = torch.empty(batch_size, 1, dtype=torch.long, device=self.device).fill_(self.SOS_token)
+        decoder_hidden = encoder_hidden
+        decoder_outputs = []
+
+        for i in range(self.MAX_LENGTH):
+            decoder_output, decoder_hidden  = self.forward_step(decoder_input, decoder_hidden)
+            decoder_outputs.append(decoder_output)
+
+            if target_tensor is not None:
+                # Teacher forcing: Feed the target as the next input
+                decoder_input = target_tensor[:, i].unsqueeze(1) # Teacher forcing
+            else:
+                # Without teacher forcing: use its own predictions as the next input
+                _, topi = decoder_output.topk(1)
+                decoder_input = topi.squeeze(-1).detach()  # detach from history as input
+
+        decoder_outputs = torch.cat(decoder_outputs, dim=1)
+        decoder_outputs = F.log_softmax(decoder_outputs, dim=-1)
+        return decoder_outputs, decoder_hidden, None # We return `None` for consistency in the training loop
+
+    def forward_step(self, input, hidden):
+        output = self.embedding(input)
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+        output = self.out(output)
+        return output, hidden
+
+def train_epoch(dataloader, encoder, decoder, encoder_optimizer,
+          decoder_optimizer, criterion):
+
+    total_loss = 0
+    for data in dataloader:
+        input_tensor, target_tensor = data
+
+        encoder_optimizer.zero_grad()
+        decoder_optimizer.zero_grad()
+
+        encoder_outputs, encoder_hidden = encoder(input_tensor)
+        decoder_outputs, _, _ = decoder(encoder_outputs, encoder_hidden, target_tensor)
+
+        loss = criterion(
+            decoder_outputs.view(-1, decoder_outputs.size(-1)),
+            target_tensor.view(-1)
+        )
+        loss.backward()
+
+        encoder_optimizer.step()
+        decoder_optimizer.step()
+
+        total_loss += loss.item()
+
+    return total_loss / len(dataloader)
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
+
+class BahdanauAttention(nn.Module):
+    def __init__(self, hidden_size):
+        super(BahdanauAttention, self).__init__()
+        self.Wa = nn.Linear(hidden_size, hidden_size)
+        self.Ua = nn.Linear(hidden_size, hidden_size)
+        self.Va = nn.Linear(hidden_size, 1)
+
+    def forward(self, query, keys):
+        scores = self.Va(torch.tanh(self.Wa(query) + self.Ua(keys)))
+        scores = scores.squeeze(2).unsqueeze(1)
+
+        weights = F.softmax(scores, dim=-1)
+        context = torch.bmm(weights, keys)
+
+        return context, weights
+
+class AttnDecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, MAX_LENGTH, SOS_token, dropout_p=0.1, device = 'cpu'):
+        super(AttnDecoderRNN, self).__init__()
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.attention = BahdanauAttention(hidden_size)
+        self.gru = nn.GRU(2 * hidden_size, hidden_size, batch_first=True)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.dropout = nn.Dropout(dropout_p)
+        self.device = device
+        self.MAX_LENGTH = MAX_LENGTH
+        self.SOS_token = SOS_token
+
+    def forward(self, encoder_outputs, encoder_hidden, target_tensor=None):
+        batch_size = encoder_outputs.size(0)
+        decoder_input = torch.empty(batch_size, 1, dtype=torch.long, device=self.device).fill_(self.SOS_token)
+        decoder_hidden = encoder_hidden
+        decoder_outputs = []
+        attentions = []
+
+        for i in range(self.MAX_LENGTH):
+            decoder_output, decoder_hidden, attn_weights = self.forward_step(
+                decoder_input, decoder_hidden, encoder_outputs
+            )
+            decoder_outputs.append(decoder_output)
+            attentions.append(attn_weights)
+
+            if target_tensor is not None:
+                # Teacher forcing: Feed the target as the next input
+                decoder_input = target_tensor[:, i].unsqueeze(1) # Teacher forcing
+            else:
+                # Without teacher forcing: use its own predictions as the next input
+                _, topi = decoder_output.topk(1)
+                decoder_input = topi.squeeze(-1).detach()  # detach from history as input
+
+        decoder_outputs = torch.cat(decoder_outputs, dim=1)
+        decoder_outputs = F.log_softmax(decoder_outputs, dim=-1)
+        attentions = torch.cat(attentions, dim=1)
+
+        return decoder_outputs, decoder_hidden, attentions
+
+
+    def forward_step(self, input, hidden, encoder_outputs):
+        embedded =  self.dropout(self.embedding(input))
+
+        query = hidden.permute(1, 0, 2)
+        context, attn_weights = self.attention(query, encoder_outputs)
+        input_gru = torch.cat((embedded, context), dim=2)
+
+        output, hidden = self.gru(input_gru, hidden)
+        output = self.out(output)
+
+        return output, hidden, attn_weights
+
+def train(train_dataloader, encoder, decoder, n_epochs, learning_rate=0.001,
+               print_every=100, plot_every=100):
+    start = time.time()
+    plot_losses = []
+    print_loss_total = 0  # Reset every print_every
+    plot_loss_total = 0  # Reset every plot_every
+
+    encoder_optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
+    decoder_optimizer = optim.Adam(decoder.parameters(), lr=learning_rate)
+    criterion = nn.NLLLoss()
+
+    for epoch in range(1, n_epochs + 1):
+        loss = train_epoch(train_dataloader, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion)
+        print_loss_total += loss
+        plot_loss_total += loss
+
+        if epoch % print_every == 0:
+            print_loss_avg = print_loss_total / print_every
+            print_loss_total = 0
+            print('%s (%d %d%%) %.4f' % (timeSince(start, epoch / n_epochs),
+                                        epoch, epoch / n_epochs * 100, print_loss_avg))
+
+        if epoch % plot_every == 0:
+            plot_loss_avg = plot_loss_total / plot_every
+            plot_losses.append(plot_loss_avg)
+            plot_loss_total = 0
+
+    showPlot(plot_losses)
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+
+def showPlot(points):
+    plt.figure()
+    fig, ax = plt.subplots()
+    # this locator puts ticks at regular intervals
+    loc = ticker.MultipleLocator(base=0.2)
+    ax.yaxis.set_major_locator(loc)
+    plt.plot(points)
+
+def printEval(encoder, decoder, dataloader, dembedder, length = 10):
+    p, f = next(iter(dataloader))
+
+    decoded_ids, decoder_attn = evaluate(encoder, decoder, f)
+
+    decrypt = list(deembed(decoded_ids, dembedder))
+    cipher = list(deembed(f, dembedder))
+    plain = list(deembed(p, dembedder))
+
+    for p,c,d in zip(plain[:length], cipher[:length], decrypt[:length]):
+        print('Plaintext: ', p.replace('{', '').replace('}', '').replace('|', ''))
+        print('Ciphertext:', c.replace('{', '').replace('}', '').replace('|', ''))
+        print('Decryption:', d.replace('{', '').replace('}', '').replace('|', ''))
+        print()
+
+def evaluate(encoder, decoder, f):
+    encoder.eval()
+    decoder.eval()
+    encoder_outputs, encoder_hidden = encoder(f)
+    decoder_outputs, decoder_hidden, decoder_attn = decoder(encoder_outputs, encoder_hidden)
+
+    _, topi = decoder_outputs.topk(1)
+    decoded_ids = topi.squeeze()
+
+    return decoded_ids, decoder_attn
+
+def showAttention(input_sentence, output_words, attentions):
+    font = {'size'   : 10}
+
+    matplotlib.rc('font', **font)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(attentions.cpu().detach().numpy(), cmap='bone')
+    fig.colorbar(cax)
+
+    # Set up axes
+    ax.set_xticklabels([''] + list(input_sentence) +
+                       ['<EOS>'], rotation=90)
+    ax.set_yticklabels([''] + list(output_words))
+
+    # Show label at every tick
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.show()
+
+
+def evaluateAndShowAttention(encoder, decoder, input_sentence, dembedder):
+    decoded_ids, decoder_attn = evaluate(encoder, decoder, input_sentence)
+    decoded_ids = decoded_ids.unsqueeze(0)
+
+    decrypt = list(deembed(decoded_ids, dembedder))[0].replace('{', '').replace('}', '').replace('|', '')
+    cipher = list(deembed(input_sentence, dembedder))[0].replace('{', '').replace('}', '').replace('|', '')
+
+    showAttention(cipher, decrypt, decoder_attn[0, :len(decrypt), :])
